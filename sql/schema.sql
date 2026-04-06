725a22b9f63a682a5f2fb430605d11fca03160a9
TheBoss7660 <satojohn861@gmail.com>
2026-04-06 08:51:14 +0300

    Initial commit

---
+++ sql/schema.sql
@@ -0,0 +1,17 @@
+CREATE TABLE sales (
+    sales DATE,
+    year INT,
+    month TEXT,
+    recession INT,
+    consumer_confidence FLOAT,
+    seasonality_weight FLOAT,
+    price INT,
+    advertising_expenditure FLOAT,
+    competition INT,
+    gdp FLOAT,
+    growth_rate FLOAT,
+    unemployment_rate FLOAT,
+    automobile_sales FLOAT,
+    vehicle_type TEXT,
+    city TEXT
+);
